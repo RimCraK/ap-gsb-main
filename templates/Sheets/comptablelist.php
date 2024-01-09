b16a52943e9cc320f9710054055c1672686c6ad7
RimCraK <146358911+RimCraK@users.noreply.github.com>
2024-01-09 11:12:55 +0100

    comptable

---
+++ templates/Sheets/comptablelist.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var iterable<\App\Model\Entity\Sheet> $sheets
+ * 
+ * 
+ *
+ */
+
+
+$identity = $this->getRequest()->getAttribute('identity');
+$identity = $identity ?? [];
+$iduser = $identity["id"]
+
+?>
+<div class="sheets index content">
+
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('state_id') ?></th>
+                    <th><?= $this->Paginator->sort('created') ?></th>
+                    <th><?= $this->Paginator->sort('modified') ?></th>
+                    <th class="display-flex"><?= $this->Paginator->sort('sheetvalidated', 'Validation') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($sheets as $sheet): ?>
+                    <tr>
+                        <td><?= $this->Number->format($sheet->id) ?></td>
+                        
+                        <td><?= $sheet->has('state') ? $this->Html->link($sheet->state->state, ['controller' => 'Sheets', 'action' => 'edit', $sheet->id]) : '' ?></td>
+
+                        <td><?= h($sheet->created) ?></td>
+                        <td><?= h($sheet->modified) ?></td>
+                        <td class="display-flex"><?php if($sheet->sheetvalidated == 1){echo "<div class='tag success'>Validated</div>";}else{echo "<div class='tag error'>Unvalidated</div>";} ?></td>
+                        <td class="actions">
+                            <?php if($sheet->state->id > 1){echo $this->Html->link(__('View'), ['action' => 'comptableview', $sheet->id]);}elseif($sheet->state->id == 1){echo $this->Html->link(__('View'), ['action' => 'comptableview', $sheet->id]);}else{echo $this->Html->link(__('Edit'), ['action' => 'clientview', $sheet->id]);}  ?>
+                            
+                            <!-- $this->Form->postLink(__('Delete'), ['action' => 'delete', $sheet->id], ['confirm' => __('Are you sure you want to delete # {0}?', $sheet->id)]) -->
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('first')) ?>
+            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('next') . ' >') ?>
+            <?= $this->Paginator->last(__('last') . ' >>') ?>
+        </ul>
+        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+    </div>
+</div>
